c5e76cf0da858a387bfa15f238f191f5ddaf692c
TONG <t13476018526@gmail.com>
2026-06-18 15:09:11 +0800

    Refactor SVIM VCF filtering with minimum support

--- SV-filter/filter_svim.sh
+++ SV-filter/filter_svim.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-# Filter SVIM VCF by SUPPORT field and PASS status
-
 if [ $# -ne 4 ]; then
     echo "Usage: $0 <input.vcf> <output.vcf> <depth_min> <depth_max>"
     exit 1
@@ -11,7 +9,34 @@ out_vcf=$2
 depth_min=$3
 depth_max=$4
 
-awk -F'\t' -v min=$depth_min -v max=$depth_max '!/^#/ && $7 == "PASS" {
-    match($8, /SUPPORT=([0-9]+)/, a);
-    if (a[1] >= min && a[1] <= max) print
-}' "$in_vcf" > "$out_vcf"
+MIN_SUPPORT=3
+
+awk -F'\t' -v min_dp="$depth_min" -v max_dp="$depth_max" -v min_supp="$MIN_SUPPORT" '
+/^#/ { print; next }
+
+$7 == "PASS" {
+    
+    supp = 0;
+    if (match($8, /SUPPORT=([0-9]+)/, a)) {
+        supp = a[1] + 0;
+    }
+
+    if (supp >= min_supp) {
+        
+        split($9, fmt, ":");
+        dp_idx = 0;
+        for (i=1; i<=length(fmt); i++) {
+            if (fmt[i] == "DP") dp_idx = i;
+        }
+
+        if (dp_idx > 0) {
+            split($10, smp, ":");
+            dp = smp[dp_idx];
+
+            if (dp == "." || (dp + 0 >= min_dp && dp + 0 <= max_dp)) {
+                print $0;
+            }
+        }
+    }
+}
+' "$in_vcf" > "$out_vcf"
